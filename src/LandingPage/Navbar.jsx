1601b1944951019c2317892a4704532c7b896454
saloni singh <saloni.singh301295@gmail.com>
2025-06-21 14:30:36 +0530

    resolve conflict

--- src/LandingPage/Navbar.jsx
+++ src/LandingPage/Navbar.jsx
@@ -43,49 +43,7 @@ function Navbar() {
 }
 
 
-import React from 'react';
 
-function Navbar() {
-    return (
-        <>
-
-
-            <nav className="navbar navbar-expand-lg border-bottom" style={{background: "#FFF"}}>
-                <div className="container p-2">
-                    <a className="navbar-brand" href="#">
-                        <img src="media/images/logo.svg" alt="logo" style={{width: "25%"}} />
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-
-
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">signUp</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-                            <li clasNames="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Product</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Support</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-
-
-        </>
-    );
-}
 
 
 export default Navbar;
--- src/LandingPage/OpenAcc.jsx
+++ src/LandingPage/OpenAcc.jsx
@@ -13,18 +13,7 @@ function OpenAcc() {
 }
 
 
-import React from 'react';
-function OpenAcc() {
-    return (
-        <div className='container m-5 mt-5'>
-            <div className='row text-center'>
-                <h2 className='mt-5 text-muted py-2'>Open a Zerodha account</h2>
-                <p className='text-muted mb-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-                <button className='p-2 btn btn-primary fs-5 mt-3' style={{width: "20%", margin:" 0 auto", fontWeight: "500"}}>Sign up Now</button>
-            </div>
-        </div>
-      );
-}
+
 
 
 export default OpenAcc;
